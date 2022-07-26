ecb6b3cabf63ee81645af7f2964d16ab573074aa
bmuthoka411 <105128683+bmuthoka411@users.noreply.github.com>
2022-07-25 22:43:29 -0400

    Update c2-variables.tf

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
@@ -10,11 +10,11 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 Instnace Type"
   type = string
-  default = "t3.micro"
+  default = "t2.micro"
 }
 
 # AWS EC2 Instance Key Pair
-variable "instance_keypair" {
+variable "terraformkey" {
   description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
   type = string
   default = "terraform-key"
