b574fbe46baf25854f8610bc3bfad5955c8d1d18
bmuthoka411 <105128683+bmuthoka411@users.noreply.github.com>
2022-07-25 22:39:33 -0400

    Update c2-ec2instance.tf

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
+  ami = "ami-08d4ac5b634553e16"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
